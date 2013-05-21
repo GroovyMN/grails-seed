83e4a6fed19ae7cd6cf2479cb8f09032e26add47
staticnull <staticnullvoid@gmail.com>
2013-05-21 10:30:50 -0500

    Added build test data plugin.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -53,6 +53,9 @@ grails.project.dependency.resolution = {
 		// runtime ":cached-resources:1.0"
 		// runtime ":yui-minify-resources:0.1.5"
 
+		compile ":console:1.2"
+		compile ":build-test-data:2.0.5"
+
 		build ":tomcat:$grailsVersion"
 
 		runtime ":database-migration:1.3.2"
