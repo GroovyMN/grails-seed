9e6d1b515f36e008b151a5d38216ce887697dd4e
staticnull <staticnullvoid@gmail.com>
2013-05-21 10:04:49 -0500

    Formatting code

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -4,60 +4,59 @@ grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 grails.project.target.level = 1.6
 grails.project.source.level = 1.6
-//grails.project.war.file = "target/${appName}-${appVersion}.war"
+// grails.project.war.file = "target/${appName}-${appVersion}.war"
 
 // uncomment (and adjust settings) to fork the JVM to isolate classpaths
-//grails.project.fork = [
+// grails.project.fork = [
 //   run: [maxMemory:1024, minMemory:64, debug:false, maxPerm:256]
-//]
+// ]
 
 grails.project.dependency.resolution = {
-    // inherit Grails' default dependencies
-    inherits("global") {
-        // specify dependency exclusions here; for example, uncomment this to disable ehcache:
-        // excludes 'ehcache'
-    }
-    log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
-    checksums true // Whether to verify checksums on resolve
-    legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
+	// Inherit Grails' default dependencies
+	inherits("global") {
+		// Specify dependency exclusions here; for example, uncomment this to disable ehcache:
+		// excludes 'ehcache'
+	}
+	log "error" // Log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+	checksums true // Whether to verify checksums on resolve
+	legacyResolve false // Whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
 
-    repositories {
-        inherits true // Whether to inherit repository definitions from plugins
+	repositories {
+		inherits true // Whether to inherit repository definitions from plugins
 
-        grailsPlugins()
-        grailsHome()
-        grailsCentral()
+		grailsPlugins()
+		grailsHome()
+		grailsCentral()
 
-        mavenLocal()
-        mavenCentral()
+		mavenLocal()
+		mavenCentral()
 
-        // uncomment these (or add new ones) to enable remote dependency resolution from public Maven repositories
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
-    }
+		// uncomment these (or add new ones) to enable remote dependency resolution from public Maven repositories
+		// mavenRepo "http://snapshots.repository.codehaus.org"
+		// mavenRepo "http://repository.codehaus.org"
+		// mavenRepo "http://download.java.net/maven/2/"
+		// mavenRepo "http://repository.jboss.com/maven2/"
+	}
 
-    dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
+	dependencies {
+		// Specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
+		// runtime 'mysql:mysql-connector-java:5.1.22'
+	}
 
-        // runtime 'mysql:mysql-connector-java:5.1.22'
-    }
+	plugins {
+		runtime ":hibernate:$grailsVersion"
+		runtime ":jquery:1.8.3"
+		runtime ":resources:1.1.6"
 
-    plugins {
-        runtime ":hibernate:$grailsVersion"
-        runtime ":jquery:1.8.3"
-        runtime ":resources:1.1.6"
+		// Uncomment these (or add new ones) to enable additional resources capabilities
+		// runtime ":zipped-resources:1.0"
+		// runtime ":cached-resources:1.0"
+		// runtime ":yui-minify-resources:0.1.5"
 
-        // Uncomment these (or add new ones) to enable additional resources capabilities
-        //runtime ":zipped-resources:1.0"
-        //runtime ":cached-resources:1.0"
-        //runtime ":yui-minify-resources:0.1.5"
+		build ":tomcat:$grailsVersion"
 
-        build ":tomcat:$grailsVersion"
+		runtime ":database-migration:1.3.2"
 
-        runtime ":database-migration:1.3.2"
-
-        compile ':cache:1.0.1'
-    }
+		compile ':cache:1.0.1'
+	}
 }
